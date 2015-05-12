f0ae8faeb7bfdafa5c4ca8d0e1f3492f72acbc51
Anna Gaughan <agaughan@gmail.com>
2015-05-12 15:04:04 +0100

    x and o working

--- scripts/main.js
+++ scripts/main.js
@@ -1,35 +1,52 @@
 $(document).ready(function() {
 
-
-// var top = ( a, b, c)
-// var middle = (d, e, f)
-// var bottom = (i, j, k)
-
 var input = "'"
 var winningCombinations = [ [a, b, c,], [ d, e, f], [d, e, f], [i, j, k], [i, j, k], [a, b, c], [b, e, j], [c, f, j], [c, e, i]]
+var lastMove = "0"
 
 // for (var i = 0; winner.length; i++) {
 //   winner[i]
 // };
-// function( ){
-//   if 
-  
-// })();
 
-// f
-
-// getting the unput and returnning it
+// getting the input and returnning it
  // function (X) {
-  $(".square").on("click", function() {
-    $(this).text('X');
+ 
+$(".square").on("click", function() {
+    // $(this).text('X');
+    if( lastMove === "X") {
+      $(this).text('0');
+      lastMove = "0" 
+    } else {
+       $(this).text('X');
+       lastMove = "X"
+    }
+      
 });
 
+  // var secondMove = $(".square").on("click", function() {
+  //   $(this).text('0');
+  //   });
 
 
-  // if($(this).text === 'x' {
-  //   $(this).text(x)
-  //   else($(this))
-  // }
+
+    // function nextMove() {
+    // if (lastMove === firstMove) {
+    //   result secondMove;
+    // }
+    // };
+
+
+
+// $(".square" ).toggleClass(function() {
+//    $(".square").on("click", function() {
+//     $(this).text('X');
+//     };
+//     function() {
+//     $(".square").on("click", function() {
+//     $(this).text('Y');
+//     });
+
+
 
 
   // nnerHTML( )
