debaf46aecb5a6f9f294b25a309c73e7e0f7a19c
Anna Gaughan <agaughan@gmail.com>
2015-05-12 13:19:15 +0100

    x working

---
+++ scripts/main.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+
+
+// var top = ( a, b, c)
+// var middle = (d, e, f)
+// var bottom = (i, j, k)
+
+var input = "'"
+var winningCombinations = [ [a, b, c,], [ d, e, f], [d, e, f], [i, j, k], [i, j, k], [a, b, c], [b, e, j], [c, f, j], [c, e, i]]
+
+// for (var i = 0; winner.length; i++) {
+//   winner[i]
+// };
+// function( ){
+//   if 
+  
+// })();
+
+// f
+
+// getting the unput and returnning it
+ // function (X) {
+  $(".square").on("click", function() {
+    $(this).text('X');
+});
+
+
+
+  // if($(this).text === 'x' {
+  //   $(this).text(x)
+  //   else($(this))
+  // }
+
+
+  // nnerHTML( )
+
+// $(".choice").valueOf()
+// return(val);
+});
+// end of document
