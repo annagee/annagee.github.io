6d0cf45da80529a783576a7451b101de8d3f22dd
Anna Gaughan <agaughan@gmail.com>
2015-05-13 20:46:57 +0100

    getting x and o winner

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@
 console.log("hi anna");
 var xMoves = [];
 var yMoves = [];
-var winningCombinations = [ [1, 2, 3,], [4, 5, 6],[7, 8, 9], [1, 5, 9], [2, 5, 8], [3, 6, 9], [3, 5, 7], [1, 4, 7] ];
+var winningCombinations = [ [1, 2, 3], [4, 5, 6],[7, 8, 9], [1, 5, 9], [2, 5, 8], [3, 6, 9], [3, 5, 7], [1, 4, 7]];
 $(document).ready(function() {
 // var winningCombinations = [ [1, 2, 3,], [4, 5, 6], [7, 8, 9], [1, 5, 9], [2, 5, 8], [3, 6, 9], [3, 5, 7], [1, 4, 7] ];
 var lastMove = "0";
@@ -14,34 +14,85 @@ $(".square").on("click", function() {
       lastMove = "0";
       var id = Number(this.id);
       yMoves.push(id);
-      checkWinner(yMoves);
+      checkWinner(yMoves, "Player O");
       console.log('yMoves', yMoves);
     }else {
       $(this).text('X');
       lastMove = "X";
       var id = Number(this.id);
       xMoves.push(id);
-      checkWinner(xMoves);
+      checkWinner(xMoves, "Player X");
       console.log('xMoves', xMoves);
         
   }
 });
 
 
-   function checkWinner (winner) {
+   function checkWinner (winner, currentPlayer) {
     var sorted = winner.sort()
     if (sorted.length < 3){
-      return false
+      console.log('not enough moves');
+      return; 
+    } 
+    for (var i = 0; i< winningCombinations.length;  i++) {
+     var winnerExists = findingWinner(sorted, winningCombinations[i])
+       console.log(sorted, winningCombinations[i]);
+        console.log(winnerExists);
+        if(winnerExists === true){ 
+        console.log(currentPlayer +' is winner');
+
     }
-    console.log('sorted', sorted);
-    for (var i = 0; i< winningCombinations.length -1; i++) {
-      for (var j = 0; j< sorted.length - 1; j++){
-        console.log(sorted)
-        if(sorted[j] !== winningCombinations[i][j]){
-      console.log(winningCombinations[i]);
-       console.log('x is winner');
-         }
+
+
+
+    };
+
+  
+
+
+    
+
+    };
+
+  
+      function findingWinner (array1, array2){
+    for (var i = 0; i< array1.length - 1;  i++) {
+      if(array1[i] !== array2[i]){
+        return false
       }
+
+
+    };
+      return true
+
+
+  };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // for (var i = 0; i< winningCombinations.length -1; i++) {
+    //   for (var j = 0; j< sorted.length - 1; j++){
+    //     console.log(sorted)
+
+
+    //     if(sorted[j] !== winningCombinations[i][j]){
+    //       console.log("jvalue",sorted[j]);
+    //       console.log("value",winningCombinations[i][j]);
+    //       continue;
+    //      }
+    //   }
+    //   console.log(currentPlayer + ' is winner');
      
          
 
@@ -54,8 +105,8 @@ $(".square").on("click", function() {
 
         //we now know the first element in the array matches - so we need to find out whether the second and third elements match too.....
              
-       }
-     }
+       
+     
     
   
 
