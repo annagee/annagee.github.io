d3801e2e0a94699c2050d04a2280d2727c7bdd43
Anna Gaughan <agaughan@gmail.com>
2015-05-13 19:48:46 +0100

    getting winner

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,12 @@
-console.log("hi anna");
 
+console.log("hi anna");
 var xMoves = [];
 var yMoves = [];
-
-
-
+var winningCombinations = [ [1, 2, 3,], [4, 5, 6],[7, 8, 9], [1, 5, 9], [2, 5, 8], [3, 6, 9], [3, 5, 7], [1, 4, 7] ];
 $(document).ready(function() {
-
-var input = "'";
-var winningCombinations = [ [1, 2, 3,], [4, 5, 6], [7, 8, 9], [1, 5, 9], [2, 5, 8], [3, 6, 9], [3, 5, 7], [1, 4, 7] ];
+// var winningCombinations = [ [1, 2, 3,], [4, 5, 6], [7, 8, 9], [1, 5, 9], [2, 5, 8], [3, 6, 9], [3, 5, 7], [1, 4, 7] ];
 var lastMove = "0";
 var result = "";
-// for (var i = 0; winner.length; i++) {
-//   winner[i]
-// };
-
-// getting the input and returnning it
- // function (X) {
- 
 $(".square").on("click", function() {
     // $(this).text('X');
     if( lastMove === "X") {
@@ -25,42 +14,50 @@ $(".square").on("click", function() {
       lastMove = "0";
       var id = Number(this.id);
       yMoves.push(id);
-      
-      // push this in to yMoves array
-    } else {
-       $(this).text('X');
-       lastMove = "X";
-       var id = Number(this.id);
-       xMoves.push(id)
-      // push this in to xMoves array
+      checkWinner(yMoves);
+      console.log('yMoves', yMoves);
+    }else {
+      $(this).text('X');
+      lastMove = "X";
+      var id = Number(this.id);
+      xMoves.push(id);
+      checkWinner(xMoves);
+      console.log('xMoves', xMoves);
+        
+  }
+});
 
+
+   function checkWinner (winner) {
+    var sorted = winner.sort()
+    if (sorted.length < 3){
+      return false
     }
+    console.log('sorted', sorted);
+    for (var i = 0; i< winningCombinations.length -1; i++) {
+      for (var j = 0; j< sorted.length - 1; j++){
+        console.log(sorted)
+        if(sorted[j] !== winningCombinations[i][j]){
+      console.log(winningCombinations[i]);
+       console.log('x is winner');
+         }
+      }
+     
+         
+
       
-});
+
+
+        // console.log("its a match");
 
 
 
-
-  // var secondMove = $(".square").on("click", function() {
-  //   $(this).text('0');
-  //   });
-
-
-
-   
-
-
-// $(".square" ).toggleClass(function() {
-//    $(".square").on("click", function() {
-//     $(this).text('X');
-//     };
-//     function() {
-//     $(".square").on("click", function() {
-//     $(this).text('Y');
-//     });
-
-
+        //we now know the first element in the array matches - so we need to find out whether the second and third elements match too.....
+             
+       }
+     }
+    
+  
 
 
 });
-// end of document
