8a14495447e1635b67e27f34bd4f2125f2115385
Anna Gaughan <agaughan@gmail.com>
2015-05-14 09:41:35 +0100

    table is centred on page

--- scripts/main.js
+++ scripts/main.js
@@ -15,100 +15,35 @@ $(".square").on("click", function() {
       var id = Number(this.id);
       yMoves.push(id);
       checkWinner(yMoves, "Player O");
-      console.log('yMoves', yMoves);
     }else {
       $(this).text('X');
       lastMove = "X";
       var id = Number(this.id);
       xMoves.push(id);
-      checkWinner(xMoves, "Player X");
-      console.log('xMoves', xMoves);
-        
-  }
-});
-
-
+      checkWinner(xMoves, "Player X");     
+      }
+    });
    function checkWinner (winner, currentPlayer) {
     var sorted = winner.sort()
     if (sorted.length < 3){
-      console.log('not enough moves');
       return; 
     } 
     for (var i = 0; i< winningCombinations.length;  i++) {
      var winnerExists = findingWinner(sorted, winningCombinations[i])
-       console.log(sorted, winningCombinations[i]);
-        console.log(winnerExists);
         if(winnerExists === true){ 
         console.log(currentPlayer +' is winner');
-
     }
-
-
-
     };
-
-  
-
-
-    
-
     };
-
-  
       function findingWinner (array1, array2){
-    for (var i = 0; i< array1.length - 1;  i++) {
-      if(array1[i] !== array2[i]){
+        for (var i = 0; i< array1.length;  i++) {
+        console.log(array1[i] !== array2[i])
+        if(array1[i] !== array2[i]){
         return false
       }
-
-
     };
       return true
-
-
   };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // for (var i = 0; i< winningCombinations.length -1; i++) {
-    //   for (var j = 0; j< sorted.length - 1; j++){
-    //     console.log(sorted)
-
-
-    //     if(sorted[j] !== winningCombinations[i][j]){
-    //       console.log("jvalue",sorted[j]);
-    //       console.log("value",winningCombinations[i][j]);
-    //       continue;
-    //      }
-    //   }
-    //   console.log(currentPlayer + ' is winner');
-     
-         
-
-      
-
-
-        // console.log("its a match");
-
-
-
-        //we now know the first element in the array matches - so we need to find out whether the second and third elements match too.....
-             
-       
-     
-    
-  
 
 
 });
