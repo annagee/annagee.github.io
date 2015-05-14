30251d7e13e7e0f95ff9c620d50ed83b9f43aa43
Anna Gaughan <agaughan@gmail.com>
2015-05-14 15:36:55 +0100

    resetbutton

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,28 @@
 
 console.log("hi anna");
 var xMoves = [];
-var yMoves = [];
+var oMoves = [];
 var winningCombinations = [ [1, 2, 3], [4, 5, 6],[7, 8, 9], [1, 5, 9], [2, 5, 8], [3, 6, 9], [3, 5, 7], [1, 4, 7]];
 $(document).ready(function() {
 // var winningCombinations = [ [1, 2, 3,], [4, 5, 6], [7, 8, 9], [1, 5, 9], [2, 5, 8], [3, 6, 9], [3, 5, 7], [1, 4, 7] ];
-var lastMove = "0";
+var lastMove = "O";
 var result = "";
 $(".square").on("click", function() {
     // $(this).text('X');
     if( lastMove === "X") {
-      $(this).text('0');
-      lastMove = "0";
+      $(this).text('O');
+      lastMove = "O";
       var id = Number(this.id);
-      yMoves.push(id);
-      checkWinner(yMoves, "Player O");
+      oMoves.push(id);
+      checkWinner(oMoves, "Player O");
+      $(this).unbind();
     }else {
       $(this).text('X');
       lastMove = "X";
       var id = Number(this.id);
       xMoves.push(id);
-      checkWinner(xMoves, "Player X");     
+      checkWinner(xMoves, "Player X"); 
+      $(this).unbind();    
       }
     });
    function checkWinner (winner, currentPlayer) {
@@ -31,19 +33,61 @@ $(".square").on("click", function() {
     for (var i = 0; i< winningCombinations.length;  i++) {
      var winnerExists = findingWinner(sorted, winningCombinations[i])
         if(winnerExists === true){ 
-        console.log(currentPlayer +' is winner');
+        alert(currentPlayer +' is winner');
     }
     };
     };
       function findingWinner (array1, array2){
-        for (var i = 0; i< array1.length;  i++) {
-        console.log(array1[i] !== array2[i])
-        if(array1[i] !== array2[i]){
+        console.log(array1)
+        console.log(array2)
+        for (var i = 0; i < array2.length; i++) {
+          if ($.inArray(array2[i], array1) !== -1 && $.inArray(array2[i+1], array1) !== -1  && $.inArray(array2[i+2], array1) !== -1){
+            return true  
+        }
+       }
         return false
       }
-    };
-      return true
-  };
+        if (array1.length >= 4){
+          $("sorted").console.log("Its a draw");
+          return true
+         console.log("Its a draw");
+
+    
+
+        }
+
+         $("#play").on("click",function() {
+         if( lastMove === "X") {
+         $(this).text('O');
+          lastMove = "O";
+          var id = Number(this.id);
+          oMoves.push(id);
+          checkWinner(oMoves, "Player O");
+          $(this).unbind();
+    }else {
+         $(this).text('X');
+         lastMove = "X";
+         var id = Number(this.id);
+         xMoves.push(id);
+        checkWinner(xMoves, "Player X"); 
+        $(this).unbind();    
+      }
+    });
 
 
+
+
+          
+          } 
+
+
+        
+        // document.getElementById("play").addEventListener("click'", function(){ alert"play game"});
+
+    
+     
+
+          // document.getElementById("play").onlick = function(){
+          //   document.getElmentById("play").innerHTML = "?";
+        // }
 });
